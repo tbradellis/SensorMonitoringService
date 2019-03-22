1b0025a31129349acc031e230859959f9379dd80
Brad Ellis <bradellis10@gmail.com>
2019-03-22 07:20:58 -0700

    removed system prints. added logging

--- src/main/java/com/bellis/oshi/ApplicationCore.java
+++ src/main/java/com/bellis/oshi/ApplicationCore.java
@@ -1,4 +1,5 @@
 package com.bellis.oshi;
+
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import oshi.SystemInfo;
@@ -7,33 +8,49 @@ import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.Sensors;
 import oshi.software.os.OperatingSystem;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class ApplicationCore {
+    private static final Logger LOGGER = Logger.getLogger("SensorLogger");
     private static final String CPU_TMP_FAHRENHEIT = "Custom/CPU_Temp/Fahrenheit";
     private static final String CPU_TMP_CELSIUS = "Custom/CPU_Temp/Celsius";
-    private static final String FAN_SPD_FAN1 = "Custom/fan_spd/fan1";
-    private static final String FAN_SPD_FAN2 = "Custom/fan_spd/fan2";
-
-
-
+    private static final String RPM_SPD_FAN1 = "Custom/fan_spd/fan1";
+    private static final String RPM_SPD_FAN2 = "Custom/fan_spd/fan2";
+    private static String log_location = "logs/sensor.log";
+    //TODO add a shutdown hook
     public static void main(String[] args){
-        System.out.println("Initializing...");
-        SystemInfo si = new SystemInfo();
+        if(args.length > 0){
+            log_location = args[0];
+        }
+        try {
+            Handler fileHandler = new FileHandler(log_location);
+            LOGGER.addHandler(fileHandler);
+            fileHandler.setFormatter(new SimpleFormatter());
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "IOException from Handler", e);
+        }
 
+        LOGGER.log(Level.INFO, "Initializing SystemInfo");
+
+        SystemInfo si = new SystemInfo();
         HardwareAbstractionLayer hal = si.getHardware();
         OperatingSystem os = si.getOperatingSystem();
 
-        System.out.println("Checking local system...");
+        LOGGER.log(Level.INFO,"Checking local system");
         printComputerSystem(hal.getComputerSystem());
 
         while(true){
-            System.out.println("Checking Sensors... ");
-            printSensorInfo(hal.getSensors());
+            sendSensorInfo(hal.getSensors());
             try {
-                Thread.sleep(10000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                LOGGER.log(Level.SEVERE, "sleep interrupted", e);
             }
         }
 
@@ -41,27 +58,29 @@ public class ApplicationCore {
     }
 
     private static void printComputerSystem(final ComputerSystem cs){
-        System.out.println("Manufacturer: " + cs.getManufacturer());
-        System.out.println("model: " + cs.getModel());
-        System.out.println("serialnumber: " + cs.getSerialNumber());
+        LOGGER.log(Level.INFO, "Manufacturer: " + cs.getManufacturer());
+        LOGGER.log(Level.INFO,"model: " + cs.getModel());
+        LOGGER.log(Level.INFO,"serialnumber: " + cs.getSerialNumber());
     }
+    //TODO separate possibly separate each metric type into different methods to track time of each call
     @Trace(dispatcher = true)
-    private static void printSensorInfo(Sensors sensors){
-        float fahrenheit = (float)(((9.0/5.0) * sensors.getCpuTemperature()) + 32);
+    private static void sendSensorInfo(Sensors sensors){
+        LOGGER.log(Level.INFO, "Sending metrics");
         float celsius = (float)sensors.getCpuTemperature();
+        float fahrenheit = (float)(((9.0/5.0) * celsius) + 32);
         NewRelic.recordMetric(CPU_TMP_CELSIUS,celsius );
         NewRelic.recordMetric(CPU_TMP_FAHRENHEIT,fahrenheit );
-        System.out.println("Sensors:");
-        System.out.format(" CPU Temperature: %.1f°C%n", sensors.getCpuTemperature());
-        System.out.format( " CPU Temperature: %.1f°F%n", fahrenheit );
+        LOGGER.log(Level.FINEST,"Checking Sensors:");
+        LOGGER.log(Level.FINEST, "CPU Temperature: %1f°C%n", celsius );
+        LOGGER.log(Level.FINEST, "CPU Temperature: %1f°F%n", fahrenheit );
+
+        //TODO this should be implemented to handle different #s of fans
+        //Possible use a case statement for 0-4
         int[] fans = sensors.getFanSpeeds();
         int fan1 = fans[0];
         int fan2 = fans[1];
-        NewRelic.recordMetric(FAN_SPD_FAN1, fan1);
-        NewRelic.recordMetric(FAN_SPD_FAN2, fan2);
-        System.out.println(" Fan Speeds: " + Arrays.toString(sensors.getFanSpeeds()));
-        System.out.format(" CPU Voltage: %.6fV%n", sensors.getCpuVoltage());
+        NewRelic.recordMetric(RPM_SPD_FAN1, fan1);
+        NewRelic.recordMetric(RPM_SPD_FAN2, fan2);
+        LOGGER.log(Level.FINEST,"Fan Speeds: ", Arrays.toString(fans));
     }
-
-
 }
